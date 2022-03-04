425ee33d0e36164cb658a00ec5535bf3996e6058
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2022-03-04 11:26:13 -0500

    db access

--- terraform/data.tf
+++ terraform/data.tf
@@ -12,3 +12,13 @@ data "terraform_remote_state" "region" {
     region = "us-east-1"
   }
 }
+
+data "terraform_remote_state" "pennsieve_postgres" {
+  backend = "s3"
+
+  config = {
+    bucket = "${var.aws_account}-terraform-state"
+    key    = "aws/${data.aws_region.current_region.name}/${var.vpc_name}/${var.environment_name}/pennsieve-postgres/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
