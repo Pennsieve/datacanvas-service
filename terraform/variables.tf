7e4096ef9a4aecb3254b94bb2699f578aa4c297a
Michael Uftring <michael.uftring@gmail.com>
2022-03-28 13:58:18 -0400

    remove DOI and DataCite references

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,20 +12,6 @@ variable "pennsieve_postgres_db" {
   default = "pennsieve_postgres"
 }
 
-variable "doi_postgres_num_connections" {
-  default = "10"
-}
-
-variable "doi_postgres_queue_size" {
-  default = "1000"
-}
-
-variable "datacite_client_username" {}
-
-variable "datacite_api_url" {}
-
-variable "datacite_pennsieve_prefix" {}
-
 variable "ecs_task_iam_role_id" {}
 
 locals {
